ccf84309d1d0cafe5b42cf025355bd3ed32ce4a2
Magnus Kirø <magnuskiro@gmail.com>
2013-03-07 13:49:00 +0100

    modified the input field for search

--- joomla/modules/mod_finder/tmpl/default.php
+++ joomla/modules/mod_finder/tmpl/default.php
@@ -17,7 +17,10 @@ $lang = JFactory::getLanguage();
 $lang->load('com_finder', JPATH_SITE);
 
 $suffix = $params->get('moduleclass_sfx');
-$output = '<input type="text" name="q" id="mod-finder-searchword" class="inputbox" size="' . $params->get('field_size', 20) . '" value="' . htmlspecialchars(JFactory::getApplication()->input->get('q', '', 'string')) . '" />';
+$output = '<input type="text" name="q" id="mod-finder-searchword" class="inputbox" size="' . $params->get('field_size', 20) . '" 
+value="' . htmlspecialchars(JFactory::getApplication()->input->get('q', '', 'string')) . 'Søkestreng"
+onClick="javascript:this.form.q.focus();this.form.q.select();"
+ />';
 $button = '';
 $label = '';
 
