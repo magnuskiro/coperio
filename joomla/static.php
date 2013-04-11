da8f458fd9881dcebe363c296084da8723a4e1a9
Magnus Kirø <magnuskiro@gmail.com>
2013-04-11 08:08:54 +0200

    added first step of the static pages thingy

---
+++ joomla/static.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <base href="http://www.coperio.no/" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="robots" content="noindex, nofollow" />
+</head
+<body>
+
+Statiske søstersider for Coperiosenteret!
+
+</body>
+</html>
