94cd9b878025e158b986596872e26e1f750e9864
Magnus Kirø <magnuskiro@gmail.com>
2013-04-11 16:53:56 +0200

    completed the functionality of the course enrollment, now the email templates await

--- joomla/components/com_jevents/views/alternative/icalevent/tmpl/enrollmentAddition/enrollment.php
+++ joomla/components/com_jevents/views/alternative/icalevent/tmpl/enrollmentAddition/enrollment.php
@@ -1,9 +1,9 @@
 <?php
 
-$validationSuccess=null; 
-
 # checking the input to see if it is correct. 
 function sanityCheck($list){
+
+	$validationSuccess=True; 
 
 #	var_dump($list);
 # TODO captcha / bot secure. 
@@ -13,21 +13,21 @@ function sanityCheck($list){
 		$e = preg_replace('/[";<>&*~|#i\[\]]/', '', $element);
 		#echo "<br>".$list."-".$element;
 		if($e == ""){
-			err($element." er ikke gyldig input.");
+			$validationSuccsess = err($element." er ikke gyldig input.");
 		}
 	}
-	
+
 	# name
 	$n = $list['name'];
 	# if var is not this or nor that or ... return false | if something is off, return false.
 	if( !is_string($n) || $n == "Navn" ){
-		err("Navnet er ikke gyldig. Gå tilbake for å endre.");
+		$validationSuccsess = err("Navnet er ikke gyldig. Gå tilbake for å endre.");
 	}
 	
 	# corp
 	$corp = $list['corp'];
 	if( !is_string($corp) || $corp == "Bedriftens navn"){
-		err("Bedriftsnavnet er ikke gyldig. Gå tilbake for å endre.");
+		$validationSuccsess = err("Bedriftsnavnet er ikke gyldig. Gå tilbake for å endre.");
 	}
 	
 	# time
@@ -35,31 +35,31 @@ function sanityCheck($list){
 #	$t = preg_quote('/<br \/>/', '', $t);
 #	echo $t; 
 	if($t == ""){
-		err("Tidspunkt må være satt");
+		$validationSuccsess = err("Tidspunkt må være satt");
 	}
 
 	# phone
 	$p = $list['phone'];
 	if( !is_numeric($p) || $p == 00000000 ){
-		err("Telefonnummeret er ikke gyldig. Gå tilbake for å endre.");
+		$validationSuccsess = err("Telefonnummeret er ikke gyldig. Gå tilbake for å endre.");
 	}
 
 	# email
 	$e = $list['email'];
 	if( !is_string($e) || $e == "navn@bedrift.no" ){
-         err("E-postadressen er ikke gyldig. Gå tilbake for å endre.");
+         $validationSuccsess = err("E-postadressen er ikke gyldig. Gå tilbake for å endre.");
     }	
 
 	# pers
 	$pr = $list['pers'];
     if( !is_numeric($pr) || $pr == 0 ){
-		err("Deltagerantallet er ikke riktig. Gå tilbake for å endre.");
+		$validationSuccsess = err("Deltagerantallet er ikke riktig. Gå tilbake for å endre.");
     }
 
 	# message
 	$m = $list['message']; 
 	if( !is_string($m) ){
-        err("Meldingen inneholder ugyldige tegn.");
+        $validationSuccsess = err("Meldingen inneholder ugyldige tegn.");
     }
 	if( $m == "Kommentarer til oss og spesielle ting vi skal ta hensyn til." ){
 		$list['message'] = ""; 
@@ -67,32 +67,68 @@ function sanityCheck($list){
 
 	if($validationSuccess){
 		return True;
-	}else{	
+	}else{
+			
 		return False; 
 	}
 }
 
 function err($msg){
 	echo "<span style='color:red'>".$msg."</span><br />";
-	$validationSuccess = False; 
+	return False; 
 }
 
 #if "email" is filled out, send email
-if (isset($_REQUEST['email'])){
+if (isset($_POST['email'])){
+
 
 	#var_dump($_POST);	
 	if ( sanityCheck($_POST) ){
 
 	#send email
-	$email = $_POST['email'] ; 
-	$subject = "Coperio Kurspåmelding" ;
-	$message = "This is the message" ;
-	$headers = 'From: Kurs@coperio.no' . "\r\n" .
-	'Reply-To: magnuskiro@coperio.no' . "\r\n" .
-	'X-Mailer: PHP/' . phpversion();
-	mail( $email,  $subject,$message,$headers );
-	echo "<span style='color:green'>Godkjent</span><br />Påmeldt til kurs, ".$_POST['time']."Takk for din interesse, du blir snart kontaktet med mer informasjon.";
 
+	# the seminar application 
+	#$email = "kurs@coperio.no" ; 
+	$email = "bedrift@coperio.no" ; 
+	# email subject 
+	$subject = "Coperio Kurspåmelding" ;
+	# message body
+	$message = "Kurspåmelding: 
+
+	Kurs: 
+	".$_POST['kurs']."
+	".substr( $_POST['time'], 0, 38 )."\n
+	
+	Kontakt person: ".$_POST['name']."\t
+	Bedrift: ".$_POST['corp']."\t
+	Telefon: ".$_POST['phone']."\t
+	E-post: ".$_POST['email']."\t
+	Antall påmeldte: ".$_POST['pers']."\t
+	Kommentarer: 
+	".$_POST['message']."\n
+	";
+	
+	# email headers.
+	$headers = 'From: bedrift@coperio.no' . "\r\n" .
+	'Reply-To: ' . "$email \r\n" .
+	'X-Mailer: PHP/' . phpversion();
+	# sending the email 
+	mail( $email,  $subject, $message, $headers );
+	echo "<span style='color:green'>Din påmelding er registrert!</span><br />Påmeldt til kurs, ".$_POST['time']."Takk for din interesse, du blir snart kontaktet med mer informasjon.";
+
+
+	# Enrollment confirmation	
+	$email = $_POST['email'];
+	$message = "Kursbekreftelse: \n'\n".$message ."\n'\n Takk for interessen. Du vil snart bli kontaktet med mer informasjon.\n\n Mvh Bedriftshelsetjenesten Coperiosenteret\n";
+	$headers = 'From: bedrift@coperio.no' . "\r\n" .
+    'Reply-To: bedrift@coperio.no' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+	mail( $email, $subject, $message, $headers );
+
+	}
+	else{
+		echo "Det er noe feil med informasjonen du har oppgitt.";
 	}
 }
 
