391e5b3c1097f264a2d44ede812ce6bd9dfffa47
Magnus Kirø <magnuskiro@gmail.com>
2013-05-23 09:11:37 +0200

    updated css with white background for sub menu items. updated sanuty chacking for enrollment

--- joomla/components/com_jevents/views/alternative/icalevent/tmpl/enrollmentAddition/enrollment.php
+++ joomla/components/com_jevents/views/alternative/icalevent/tmpl/enrollmentAddition/enrollment.php
@@ -3,7 +3,7 @@
 # checking the input to see if it is correct. 
 function sanityCheck($list){
 
-	$validationSuccess=True; 
+	$validationSuccess=true; 
 
 #	var_dump($list);
 # TODO captcha / bot secure. 
@@ -13,21 +13,28 @@ function sanityCheck($list){
 		$e = preg_replace('/[";<>&*~|#i\[\]]/', '', $element);
 		#echo "<br>".$list."-".$element;
 		if($e == ""){
-			$validationSuccsess = err($element." er ikke gyldig input.");
+			$validationSuccsess = false; 
+			err($element." er ikke gyldig input.");
 		}
 	}
 
 	# name
 	$n = $list['name'];
 	# if var is not this or nor that or ... return false | if something is off, return false.
-	if( !is_string($n) || $n == "Navn" ){
-		$validationSuccsess = err("Navnet er ikke gyldig. Gå tilbake for å endre.");
+	if( !is_string($n) || $n == "Navn" 
+		|| preg_match("/.*Navn.*/", $e)
+	){
+		$validationSuccsess = false;
+		err("Navnet er ikke gyldig. Gå tilbake for å endre.");
 	}
 	
 	# corp
 	$corp = $list['corp'];
-	if( !is_string($corp) || $corp == "Bedriftens navn"){
-		$validationSuccsess = err("Bedriftsnavnet er ikke gyldig. Gå tilbake for å endre.");
+	if( !is_string($corp) || $corp == "Bedriftens navn"
+		|| preg_match("/.*Bedriftens navn.*/", $e)
+	){
+		$validationSuccsess = false;
+		err("Bedriftsnavnet er ikke gyldig. Gå tilbake for å endre.");
 	}
 	
 	# time
@@ -35,47 +42,53 @@ function sanityCheck($list){
 #	$t = preg_quote('/<br \/>/', '', $t);
 #	echo $t; 
 	if($t == ""){
-		$validationSuccsess = err("Tidspunkt må være satt");
+		$validationSuccsess = false;
+		err("Tidspunkt må være satt");
 	}
 
 	# phone
 	$p = $list['phone'];
 	if( !is_numeric($p) || $p == 00000000 ){
-		$validationSuccsess = err("Telefonnummeret er ikke gyldig. Gå tilbake for å endre.");
+		$validationSuccsess = false;
+		err("Telefonnummeret er ikke gyldig. Gå tilbake for å endre.");
 	}
 
 	# email
 	$e = $list['email'];
-	if( !is_string($e) || $e == "navn@bedrift.no" ){
-         $validationSuccsess = err("E-postadressen er ikke gyldig. Gå tilbake for å endre.");
+	if( !is_string($e) 
+		|| $e == "navn@bedrift.no"
+		|| preg_match("/.*navn@bedrift.no.*/", $e)
+        || !filter_var($list['email'], FILTER_VALIDATE_EMAIL)
+        || !preg_match("/^[A-Za-z0-9]*@[A-Za-z0-9]*\.[a-z]{2,4}$/", $e)
+	){
+        $validationSuccsess = false;
+		err("E-postadressen er ikke gyldig. Gå tilbake for å endre.");
     }	
 
 	# pers
 	$pr = $list['pers'];
     if( !is_numeric($pr) || $pr == 0 ){
-		$validationSuccsess = err("Deltagerantallet er ikke riktig. Gå tilbake for å endre.");
+		$validationSuccsess = false;
+		err("Deltagerantallet er ikke gyldig input. Gå tilbake for å endre.");
     }
 
 	# message
 	$m = $list['message']; 
 	if( !is_string($m) ){
-        $validationSuccsess = err("Meldingen inneholder ugyldige tegn.");
+        $validationSuccsess = false;
+		err("Meldingen inneholder ugyldige tegn.");
     }
-	if( $m == "Kommentarer til oss og spesielle ting vi skal ta hensyn til." ){
+	if( $m == "Kommentarer til oss og spesielle ting vi skal ta hensyn til." 
+		|| preg_match("/.*Kommentarer til oss og spesielle ting vi skal ta hensyn til\..*/", $e)
+	){
 		$list['message'] = ""; 
 	}
 
-	if($validationSuccess){
-		return True;
-	}else{
-			
-		return False; 
-	}
+	return $validationSuccsess; 
 }
 
 function err($msg){
 	echo "<span style='color:red'>".$msg."</span><br />";
-	return False; 
 }
 
 #if "email" is filled out, send email
